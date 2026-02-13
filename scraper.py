04450a30cf5c4e6fead8a05ffc619b3c27fdc3ef
Oreofe Adenubi <144061593+oreofemoses@users.noreply.github.com>
2026-02-13 10:47:59 +0100

    Log chat IDs from environment variable
    Added print statements to log chat IDs and their list.

--- scraper.py
+++ scraper.py
@@ -17,7 +17,9 @@ TELEGRAM_BOT_TOKEN = os.getenv('TELEGRAM_BOT_TOKEN')
 # Support multiple chat IDs (comma-separated in environment variable)
 # Example: TELEGRAM_CHAT_IDS="123456789,987654321"
 chat_ids_str = os.getenv('TELEGRAM_CHAT_IDS')
+print(chat_ids_str)
 TELEGRAM_CHAT_IDS = chat_ids_str.split(',') if chat_ids_str else []
+print(TELEGRAM_CHAT_IDS)
 
 # Nigerian timezone (UTC+1)
 NIGERIAN_TZ = timezone(timedelta(hours=1))
